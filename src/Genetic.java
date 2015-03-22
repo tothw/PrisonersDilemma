7cd452a183828a6b476627f792a51431f85c32d4
William Justin Toth <tothw@uwindsor.ca>
2015-03-22 16:50:19 -0400

    Agent that plays Genetic Algorithm generated strat

---
+++ src/Genetic.java
@@ -0,0 +1,30 @@
+
+public class Genetic extends Agent{
+	
+	public Genetic(int memoryDepth, String chromosome) throws Exception {
+		super(memoryDepth, chromosome);
+	}
+
+	@Override
+	public void establishPremises() {
+		for(int i = strategy.length; i<strategy.length + premises.length; ++i){
+			System.out.println(i);
+			System.out.println(chromosome);
+			premises[i - strategy.length] = (int)(chromosome.charAt(i) - '0');
+		}
+	}
+
+	@Override
+	public void createPlan() {
+		for(int i = 0; i<strategy.length; ++i){
+			strategy[i] = (int)(chromosome.charAt(i) - '0');
+		}
+	}
+
+	@Override
+	public String getName() {
+		return "Genetic (evolved from Genetic algorithm)";
+	}
+
+	
+}
